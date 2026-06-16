c440144354544c86a3415aaa33ba9ce3e2493217
Roman-0105 <romanyukin01@gmail.com>
2026-06-16 16:01:11 +0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Замените на ваши данные из Supabase: Settings → API
-const SUPABASE_URL = 'YOUR_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://ingbzhyrasqhhcstxpsg.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImluZ2J6aHlyYXNxaGhjc3R4cHNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1OTYxMDEsImV4cCI6MjA5NzE3MjEwMX0.1JgENEk5uCLILV-ddnJbTtUqfBInN9VeA1iE673rYlo';
